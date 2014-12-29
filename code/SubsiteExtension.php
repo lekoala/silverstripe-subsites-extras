c7491c973680ba8851c43e6f20a1efb51adc264b
Thomas <thomas@lekoala.be>
2014-12-29 11:05:41 +0100

    default subsite title applied to config

--- code/SubsiteExtension.php
+++ code/SubsiteExtension.php
@@ -12,16 +12,18 @@ class SubsiteExtension extends DataExtension
         'BaseFolder' => 'Varchar(50)'
     );
     private static $admin_default_permissions = array();
+    private static $_current_siteconfig_cache = array();
 
     function onBeforeWrite()
     {
         parent::onBeforeWrite();
 
-        // create the base folder
+        // Create the base folder
         if (!$this->owner->BaseFolder && $this->owner->Title) {
             $filter                  = new URLSegmentFilter();
             $this->owner->BaseFolder = $filter->filter($this->owner->getTitle());
-            $this->owner->BaseFolder = str_replace(' ','',ucwords(str_replace('-', ' ', $this->owner->BaseFolder)));
+            $this->owner->BaseFolder = str_replace(' ', '',
+                ucwords(str_replace('-', ' ', $this->owner->BaseFolder)));
         }
     }
 
@@ -29,12 +31,22 @@ class SubsiteExtension extends DataExtension
     {
         parent::onAfterWrite();
 
-
+        // TODO: should test if this is needed or not
         if (!$this->owner->ID) {
             return;
         }
 
-        //make sure we have a group for this subsite
+        // Apply the subsite title to config
+        $siteconfig = $this->getSiteConfig();
+        if ($siteconfig) {
+            if ($siteconfig->Title == _t('Subsite.SiteConfigTitle',
+                    'Your Site Name') && $this->owner->Title) {
+                $siteconfig->Title = $this->owner->Title;
+                $siteconfig->write();
+            }
+        }
+
+        // Make sure we have a group for this subsite
         $groupName  = $this->getAdministratorGroupName();
         $groupCount = DB::query('SELECT * FROM Group_Subsites WHERE SubsiteID = '.$this->owner->ID)->numRecords();
         if (!$groupCount) {
@@ -45,7 +57,7 @@ class SubsiteExtension extends DataExtension
 
             $group->Subsites()->add($this->owner);
 
-            //apply default permissions to this group
+            // Apply default permissions to this group
             $codes               = array_unique(array_keys(Permission::get_codes(false)));
             $default_permissions = Config::inst()->get('SubsiteExtension',
                 'admin_default_permissions');
@@ -66,8 +78,44 @@ class SubsiteExtension extends DataExtension
         $fields->addFieldToTab('Root.Configuration', new TextField('BaseFolder'));
     }
 
+    /**
+     * Get the administrator group name based on subsite Title
+     * 
+     * @return string
+     */
     function getAdministratorGroupName()
     {
         return 'Administrators '.$this->owner->Title;
+    }
+
+    /**
+     * Return a siteconfig for this subsite
+     * 
+     * @return \SiteConfig
+     */
+    function getSiteConfig()
+    {
+        if (!$this->owner->ID) {
+            return;
+        }
+        if (isset(self::$_current_siteconfig_cache[$this->owner->ID])) {
+            return self::$_current_siteconfig_cache[$this->owner->ID];
+        }
+        Subsite::$disable_subsite_filter = true;
+        $sc                              = SiteConfig::get()->filter('SubsiteID',
+                $this->owner->ID)->first();
+        Subsite::$disable_subsite_filter = false;
+
+        if (!$sc) {
+            $sc            = new SiteConfig();
+            $sc->SubsiteID = $this->owner->ID;
+            $sc->Title     = _t('Subsite.SiteConfigTitle', 'Your Site Name');
+            $sc->Tagline   = _t('Subsite.SiteConfigSubtitle',
+                'Your tagline here');
+            $sc->write();
+        }
+
+        self::$_current_siteconfig_cache[$this->owner->ID] = $sc;
+        return $sc;
     }
 }
