caaf067d5ebe29000c7da481cd9af069092db3de
Thomas Portelange <thomas.portelange@boomerang.be>
2015-03-24 12:08:24 +0100

    fluent url fix for subsite

--- _config.php
+++ _config.php
@@ -3,4 +3,7 @@
 File::remove_extension('FileSubsites');
 File::add_extension('SubsiteFileExtension');
 
-
+if(class_exists('Fluent')) {
+    SiteTree::remove_extension('FluentSiteTree');
+    SiteTree::add_extension('FluentSiteTreeSubsiteFix');
+}
