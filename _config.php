cbc5f7575b1f50899430d47bb2abc8db7231f4d7
Thomas Portelange <thomas.portelange@boomerang.be>
2015-03-24 12:28:23 +0100

    smarter way to handle url segment field

--- _config.php
+++ _config.php
@@ -2,8 +2,3 @@
 
 File::remove_extension('FileSubsites');
 File::add_extension('SubsiteFileExtension');
-
-if(class_exists('Fluent')) {
-    SiteTree::remove_extension('FluentSiteTree');
-    SiteTree::add_extension('FluentSiteTreeSubsiteFix');
-}
--- code/FluentSiteTreeSubsiteFix.php
+++ code/FluentSiteTreeSubsiteFix.php
@@ -8,78 +8,8 @@
  *
  * @author lekoala
  */
-class FluentSiteTreeSubsiteFix extends FluentExtension
+class FluentSiteTreeSubsiteFix extends DataExtension
 {
-
-    public function MetaTags(&$tags)
-    {
-        $tags .= $this->owner->renderWith('FluentSiteTree_MetaTags');
-    }
-
-    public function onBeforeWrite()
-    {
-        // Fix issue with MenuTitle not containing the correct translated value
-        $this->owner->setField('MenuTitle', $this->owner->MenuTitle);
-
-        parent::onBeforeWrite();
-    }
-
-    /**
-     * Ensure that the controller is correctly initialised
-     *
-     * @param ContentController $controller
-     */
-    public function contentcontrollerInit($controller)
-    {
-        Fluent::install_locale();
-    }
-
-    public function updateRelativeLink(&$base, &$action)
-    {
-
-        // Don't inject locale to subpages
-        if ($this->owner->ParentID && SiteTree::config()->nested_urls) {
-            return;
-        }
-
-        // For blank/temp pages such as Security controller fallback to querystring
-        $locale = Fluent::current_locale();
-        if (!$this->owner->exists()) {
-            $base = Controller::join_links($base,
-                    '?'.Fluent::config()->query_param.'='.urlencode($locale));
-            return;
-        }
-
-        // Check if this locale is the default for its own domain
-        $domain = Fluent::domain_for_locale($locale);
-        if ($locale === Fluent::default_locale($domain)) {
-            // For home page in the default locale, do not alter home url
-            if ($base === null) return;
-
-            // For all pages on a domain where there is only a single locale,
-            // then the domain itself is sufficient to distinguish that domain
-            // See https://github.com/tractorcow/silverstripe-fluent/issues/75
-            $domainLocales = Fluent::locales($domain);
-            if (count($domainLocales) === 1) return;
-        }
-
-        // Simply join locale root with base relative URL
-        $localeURL = Fluent::alias($locale);
-        $base      = Controller::join_links($localeURL, $base);
-    }
-
-    public function LocaleLink($locale)
-    {
-
-        // For blank/temp pages such as Security controller fallback to querystring
-        if (!$this->owner->exists()) {
-            $url = Controller::curr()->getRequest()->getURL();
-            return Controller::join_links($url,
-                    '?'.Fluent::config()->query_param.'='.urlencode($locale));
-        }
-
-        return parent::LocaleLink($locale);
-    }
 
     public function updateCMSFields(FieldList $fields)
     {
@@ -89,11 +19,41 @@ class FluentSiteTreeSubsiteFix extends FluentExtension
             if (class_exists('Subsite') && $this->owner->SubsiteID) {
                 $baseUrl = Director::protocol().$this->owner->Subsite()->domain().'/';
             }
-            $baseLink   = Director::absoluteURL(Controller::join_links(
+            $baseLink = Director::absoluteURL(Controller::join_links(
                         $baseUrl, Fluent::alias(Fluent::current_locale()), '/'
             ));
-            $urlsegment = $fields->dataFieldByName('URLSegment');
+
+            /* @var SiteTreeURLSegmentField $originalUrlSegment */
+            $originalUrlSegment = $fields->dataFieldByName("URLSegment");
+
+            $urlsegment = new SiteTreeURLSegmentFieldFixed("URLSegment",
+                $originalUrlSegment->Title());
+            $urlsegment->setHelpText($originalUrlSegment->getHelpText());
+
+            $fields->replaceField('URLSegment', $urlsegment);
             $urlsegment->setURLPrefix($baseLink);
+            $urlsegment->setCannotSetPrefix(true);
         }
+    }
+}
+
+/**
+ * Prevent any other modifications by Fluent
+ */
+class SiteTreeURLSegmentFieldFixed extends SiteTreeURLSegmentField
+{
+    protected $cannot_set_prefix = false;
+
+    public function setCannotSetPrefix($v)
+    {
+        $this->cannot_set_prefix = $v;
+    }
+
+    public function setURLPrefix($url)
+    {
+        if ($this->cannot_set_prefix) {
+            return;
+        }
+        parent::setURLPrefix($url);
     }
 }
